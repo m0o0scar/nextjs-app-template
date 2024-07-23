343697365619ca3055336488f065cace49942793
Oscar <tangqh@sea.com>
2024-07-23 11:34:24 +0800

    fix(auth.js): ignore auth related routes when checking auth status

--- src/auth.ts
+++ src/auth.ts
@@ -126,9 +126,13 @@ export const { handlers, auth, signIn, signOut } = NextAuth({
       return session;
     },
 
-    authorized: async ({ auth }) => {
-      // Protecting resources with middleware
-      // https://authjs.dev/getting-started/session-management/protecting#nextjs-middleware
+    // Protecting resources with middleware
+    // https://authjs.dev/getting-started/session-management/protecting#nextjs-middleware
+    authorized: async ({ request, auth }) => {
+      // Ignore auth related routes
+      const url = new URL(request.url);
+      if (url.pathname.startsWith('/api/auth/')) return true;
+
       // Logged in users are authenticated, otherwise redirect to login page
       return !!auth;
     },
